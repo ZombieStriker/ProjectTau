4de7d4ddfd92c2e22f52e205770d49180050c475
Zombie_Striker <dannyboyhn@yahoo.com>
2021-06-11 02:42:34 -0400

    push

--- Tau/src/main/java/com/projecttau/bootstrap/Bootstrap.java
+++ Tau/src/main/java/com/projecttau/bootstrap/Bootstrap.java
@@ -1,4 +1,10 @@
 package com.projecttau.bootstrap;
 
+import net.minestom.server.MinecraftServer;
+
 public class Bootstrap {
+
+    public static void main(String... args){
+        MinecraftServer server = new MinecraftServer();
+    }
 }
