87c7a85bd83d1ded7feae79d2f56977b78f8ce87
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-06-11 10:06:08 +0300

    Fix project structure

--- src/main/java/com/projecttau/bootstrap/Bootstrap.java
+++ src/main/java/com/projecttau/bootstrap/Bootstrap.java
@@ -4,7 +4,7 @@ import net.minestom.server.MinecraftServer;
 
 public class Bootstrap {
 
-    public static void main(String... args){
+    public static void main(String... args) {
         MinecraftServer server = new MinecraftServer();
     }
 }
